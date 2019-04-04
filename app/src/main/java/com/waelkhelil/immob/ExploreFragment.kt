8a3f0227ab402bc3c7d26338884ce377d7f70177
wael <khelilwael@gmail.com>
2019-04-04 17:58:06 +0100

    link bottom navigation with the navigation component

--- app/src/main/java/com/waelkhelil/immob/ExploreFragment.kt
+++ app/src/main/java/com/waelkhelil/immob/ExploreFragment.kt
@@ -7,14 +7,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class AddPostFragment : Fragment() {
+class ExploreFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_post, container, false)
+        return inflater.inflate(R.layout.fragment_explore, container, false)
     }
 
 
