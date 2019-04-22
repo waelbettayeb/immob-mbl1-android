ab6bb6c4c47cc19b1cbcc9ea4a9aaff5e2e51fb9
wael <khelilwael@gmail.com>
2019-04-22 09:12:01 +0100

    migrate to navigation component ktx

--- app/src/main/java/com/waelkhelil/immob/MainActivity.kt
+++ app/src/main/java/com/waelkhelil/immob/MainActivity.kt
@@ -3,28 +3,17 @@ package com.waelkhelil.immob
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-        private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_explore -> {
-                findNavController(this, R.id.nav_host_fragment).navigate(R.id.ExploreFragment)
-            }
-            R.id.navigation_search -> {
-                findNavController(this, R.id.nav_host_fragment).navigate(R.id.SearchFragment)
-            }
-            R.id.navigation_add-> {
-                findNavController(this, R.id.nav_host_fragment).navigate(R.id.AddPostFragment)
-            }
-        }
-        true
-    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val lBottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
-        lBottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        val navController = findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(lBottomNavigationView, navController)
     }
 }
